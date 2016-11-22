3619e58a244b8afe768abf048613cf32ebb6eb6b
maximebisantz <maxime.bisantz@gmail.com>
2016-11-22 18:16:58 +0100

    forms

--- tutorial_thinkster/tutorial_a-better-way-to-learn-angular/public/services/app.spec.js
+++ tutorial_thinkster/tutorial_a-better-way-to-learn-angular/public/services/app.spec.js
@@ -54,4 +54,58 @@ describe('Unit testing for myServiceModule', function(){
 	});
 	
 	
+});
+
+/** -------------------------
+ * SECOND PART OF THE TUTORIAL 
+ *____________________________*/
+
+describe('Second part of the tutorial', function(){
+	
+	var messages, listCtrl, postCtrl;
+	
+	beforeEach(module('thinksterService'));
+	
+	beforeEach(function(){
+		
+		inject(function($injector, $rootScope, $controller){
+			messages = $injector.get('messages');
+			listCtrl = $controller('ListCtrl');
+			postCtrl = $controller('PostCtrl');
+		}) 
+		
+	})
+	
+	it('should have the message service', function(){
+		
+		// Spy on the method, but calls the real function
+		spyOn(messages, 'add').and.callThrough();
+
+		messages.add('hello');
+		
+		expect(messages.add).toHaveBeenCalled();
+		
+		console.log(messages.list);
+		
+		expect(listCtrl.messages).toBeDefined();
+		expect(listCtrl.messages[0].text).toBe('hello');
+		
+	});
+	
+	it('should have the functionnality of the service', function(){
+		
+		spyOn(messages, 'add'); //will mock the call and not go through the real one.
+		
+		postCtrl.addMessage('Tibet is a nice place to be');
+		
+		expect(messages.add).toHaveBeenCalledWith('Tibet is a nice place to be');
+		
+		
+	})
+	
 })
+
+
+
+
+
